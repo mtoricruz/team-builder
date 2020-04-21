a8c0921e8a070c6b78c80d353ac2e00ea71fd4b9
toricruzmendiola <mtoricruz@gmail.com>
2020-04-21 13:29:57 -0700

    checkpoint save. form renders correctly, just need to make it so the team members show up when you submit the form

---
+++ team-builder/src/App.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import Form from './components/Form'
+import './App.css';
+
+
+// the shape of the state that drives the form
+const initialFormValues = {
+  name: '',
+  email: '',
+  role: ''
+}
+
+function App() {
+  const [friends, setFriends] = useState(initialFormValues)
+
+  // STEP 1 - WE NEED TO HOLD ALL VALUES OF THE FORM!
+  const [formValues, setFormValues] = useState(initialFormValues)
+
+  const onInputChange = evt => {
+    // STEP 4 IMPLEMENT A CHANGE HANDLER (works for inputs and dropdowns)
+    // which can change the state of inputs of type text
+
+    // a) pull the name of the input from the event object
+    const name = evt.target.name // either 'name', 'email', or 'role'
+    // b) pull the value of the input from the event object
+    const value = evt.target.value // the current value of the input
+    // c) set a new state for the whole form
+
+    setFormValues({
+      //copy over all the properties from formValues
+      ...formValues,
+      [name]: value, // <-- NOT AN ARRAY
+    })
+
+  }
+
+  const onSubmit = evt => {
+    // STEP 5 - IMPLEMENT A SUBMIT HANDLER
+
+    // a) don't allow the browser to reload using .preventDefault()!
+    evt.preventDefault()
+
+    const newFriend = {
+      //id: uuid()
+      name: formValues.name,
+      email: formValues.email,
+      role: formValues.role
+    }
+    // c) update the list of Team Members in state with the new Team Member
+    setFriends([ ...friends, newFriend ])
+    // d) optionally clear the form
+    setFormValues(initialFormValues)
+  }
+
+  return (
+    <div className="App">
+      <header><h1>List o'Team Members</h1></header>
+      {/* {
+        friends.map(friend => {
+          return (
+            <Friend key={friend.id} />
+          )
+        })
+      } */}
+
+      <Form
+        // STEP 2 - FORM WANTS ITS FOOD!
+        // check implementation of FriendForm
+        // to see what props it expects
+        values={formValues}
+        onInputChange={onInputChange}
+      />
+    </div>
+  );
+}
+
+export default App;
